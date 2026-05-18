3560fc1e710ae438737a00a3fa86a7ac95504410
DanOren11 <danoren545@gmail.com>
2026-05-18 19:52:59 +0300

    Fixing layout

--- Argo_cd/lab_01/index.js
+++ Argo_cd/lab_01/index.js
@@ -2,7 +2,7 @@ import { createServer } from 'node:http';
 
 const server = createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('This is My Now it should work\n');
+  res.end('We chacked it and its work now lets test if argo cd actually updated it live?! lets test it out....\n');
 });
 
 server.listen(3000, '0.0.0.0', () => {
